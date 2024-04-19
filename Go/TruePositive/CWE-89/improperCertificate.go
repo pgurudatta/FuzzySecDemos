b18ab450e705b75d15852e56970e507336f892ee
pgurudatta <89186176+pgurudatta@users.noreply.github.com>
2024-04-19 17:33:31 +0530

    Create improperCertificate.go

---
+++ Go/TruePositive/CWE-89/improperCertificate.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	url := "https://example.com"
+
+	// Disable certificate validation (INSECURE - DO NOT USE IN PRODUCTION)
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+
+	// Send HTTP GET request to the URL
+	resp, err := client.Get(url)
+	if err != nil {
+		log.Fatalf("Failed to send GET request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Read response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Failed to read response body: %v", err)
+	}
+
+	fmt.Printf("Response Status: %s\n", resp.Status)
+	fmt.Printf("Response Body: %s\n", body)
+}

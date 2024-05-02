43593ff4eba666c04b3874fb08e012201bb539dc
pgurudatta <89186176+pgurudatta@users.noreply.github.com>
2024-05-02 14:55:19 +0530

    Update test.go

--- Go/TruePositive/CWE-89/test.go
+++ Go/TruePositive/CWE-89/test.go
@@ -2,47 +2,29 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"strings"
+	"net/http"
 
-	"github.com/xmlpath/path"
+	"github.com/ChrisTrenkamp/goxpath"
+	"github.com/ChrisTrenkamp/goxpath/tree"
 )
 
-func searchUsers(xmlFile string, username string) ([]string, error) {
-	data, err := ioutil.ReadFile(xmlFile)
-	if err != nil {
-		return nil, err
-	}
+func main() {}
 
-	expr := fmt.Sprintf("//user[username='%s']", username)
-	root, err := path.Parse(string(data))
-	if err != nil {
-		return nil, err
-	}
+func processRequest(r *http.Request, doc tree.Node) {
+	r.ParseForm()
+	username := r.Form.Get("username")
 
-	var results []string
-	iter := root.Iter("//user")
-	for node := iter.Next(); node != nil; node = iter.Next() {
-		username, ok := node.SelectElements("//username")[0].String()
-		if ok && username == username {
-			results = append(results, username)
-		}
-	}
+	// BAD: User input used directly in an XPath expression
+	xPath := goxpath.MustParse("//users/user[login/text()='" + username + "']/home_dir/text()")
+	unsafeRes, _ := xPath.ExecBool(doc)
+	fmt.Println(unsafeRes)
 
-	return results, nil
-}
-
-func main() {
-	username := "admin"
-	users, err := searchUsers("users.xml", username)
-	if err != nil {
-		fmt.Println("Error searching users:", err)
-		return
+	// GOOD: Value of parameters is defined here instead of directly in the query
+	opt := func(o *goxpath.Opts) {
+		o.Vars["username"] = tree.String(username)
 	}
-
-	if len(users) > 0 {
-		fmt.Println("Found users:", strings.Join(users, ", "))
-	} else {
-		fmt.Println("User not found.")
-	}
+	// GOOD: Uses parameters to avoid including user input directly in XPath expression
+	xPath = goxpath.MustParse("//users/user[login/text()=$username]/home_dir/text()")
+	safeRes, _ := xPath.ExecBool(doc, opt)
+	fmt.Println(safeRes)
 }

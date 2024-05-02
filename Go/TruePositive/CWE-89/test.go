a9e83a6ecca15d726fbdb7eda8f5b524a0c40dd9
pgurudatta <89186176+pgurudatta@users.noreply.github.com>
2024-05-02 15:13:13 +0530

    Update test.go

--- Go/TruePositive/CWE-89/test.go
+++ Go/TruePositive/CWE-89/test.go
@@ -1,30 +1,16 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
 	"fmt"
-	"net/http"
-
-	"github.com/ChrisTrenkamp/goxpath"
-	"github.com/ChrisTrenkamp/goxpath/tree"
 )
 
-func main() {}
-
-func processRequest(r *http.Request, doc tree.Node) {
-	r.ParseForm()
-	username := r.Form.Get("username")
-
-	// BAD: User input used directly in an XPath expression
-	xPath := goxpath.MustParse("//users/user[login/text()='" + username + "']/home_dir/text()")
-	unsafeRes, _ := xPath.ExecBool(doc)
-	fmt.Println(unsafeRes)
-
-	// GOOD: Value of parameters is defined here instead of directly in the query
-	opt := func(o *goxpath.Opts) {
-		o.Vars["username"] = tree.String(username)
+func main() {
+	//Generate Private Key
+	pvk, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		fmt.Println(err)
 	}
-	// GOOD: Uses parameters to avoid including user input directly in XPath expression
-	xPath = goxpath.MustParse("//users/user[login/text()=$username]/home_dir/text()")
-	safeRes, _ := xPath.ExecBool(doc, opt)
-	fmt.Println(safeRes)
+	fmt.Println(pvk)
 }

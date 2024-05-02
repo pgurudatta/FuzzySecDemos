85fa72addf7b970a56450e12d3b7be0f003f8957
pgurudatta <89186176+pgurudatta@users.noreply.github.com>
2024-05-02 15:03:00 +0530

    Update xpathInjection.go

--- Go/TruePositive/CWE-89/xpathInjection.go
+++ Go/TruePositive/CWE-89/xpathInjection.go
@@ -1,72 +1,30 @@
 package main
 
 import (
-	"encoding/xml"
 	"fmt"
-	"log"
 	"net/http"
-	"strings"
+
+	"github.com/ChrisTrenkamp/goxpath"
+	"github.com/ChrisTrenkamp/goxpath/tree"
 )
 
-// Example XML data structure
-type User struct {
-	XMLName  xml.Name `xml:"user"`
-	Username string   `xml:"username"`
-	Password string   `xml:"password"`
-}
+func main() {}
 
-func main() {
-	http.HandleFunc("/login", handleLogin)
-	http.ListenAndServe(":8080", nil)
-}
+func processRequest(r *http.Request, doc tree.Node) {
+	r.ParseForm()
+	username := r.Form.Get("username")
 
-func handleLogin(w http.ResponseWriter, r *http.Request) {
-	// Get username and password from request parameters
-	username := r.FormValue("username")
-	password := r.FormValue("password")
+	// BAD: User input used directly in an XPath expression
+	xPath := goxpath.MustParse("//users/user[login/text()='" + username + "']/home_dir/text()")
+	unsafeRes, _ := xPath.ExecBool(doc)
+	fmt.Println(unsafeRes)
 
-	// Construct XPath query using user-supplied data
-	xpathQuery := fmt.Sprintf("/users/user[username/text()='%s' and password/text()='%s']", username, password)
-
-	// Simulate querying XML data based on the constructed XPath query
-	xmlData := `<users>
-					<user>
-						<username>admin</username>
-						<password>strongPassword123</password>
-					</user>
-					<user>
-						<username>john</username>
-						<password>password123</password>
-					</user>
-			   </users>`
-
-	// Unmarshal XML data into User struct
-	var users []User
-	err := xml.Unmarshal([]byte(xmlData), &users)
-	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
+	// GOOD: Value of parameters is defined here instead of directly in the query
+	opt := func(o *goxpath.Opts) {
+		o.Vars["username"] = tree.String(username)
 	}
-
-	// Find user based on XPath query
-	var foundUser User
-	for _, user := range users {
-		if matchesXPath(user, xpathQuery) {
-			foundUser = user
-			break
-		}
-	}
-
-	// Validate if user was found
-	if foundUser.Username != "" {
-		fmt.Fprintf(w, "Login successful for user: %s\n", foundUser.Username)
-	} else {
-		fmt.Fprintf(w, "Login failed: User not found\n")
-	}
-}
-
-// Function to check if the given user matches the provided XPath query
-func matchesXPath(user User, xpathQuery string) bool {
-	// Use strings.Contains for a simple check (vulnerable to injection)
-	return strings.Contains(xpathQuery, user.Username) && strings.Contains(xpathQuery, user.Password)
+	// GOOD: Uses parameters to avoid including user input directly in XPath expression
+	xPath = goxpath.MustParse("//users/user[login/text()=$username]/home_dir/text()")
+	safeRes, _ := xPath.ExecBool(doc, opt)
+	fmt.Println(safeRes)
 }

91be1bd6aaaece28192fa6b7051ccd1949e31978
pgurudatta <89186176+pgurudatta@users.noreply.github.com>
2024-05-02 13:04:26 +0530

    Update emailContentInjection.go

--- Go/TruePositive/CWE-89/emailContentInjection.go
+++ Go/TruePositive/CWE-89/emailContentInjection.go
@@ -1,35 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 	"net/smtp"
 )
 
-func sendEmail(recipient string, subject string, body string) error {
-  // ... configuration details for email server omitted for brevity
-
-  msg := "From: noreply@yourcompany.com\r\n"
-  msg += "To: " + recipient + "\r\n"
-  msg += "Subject: " + subject + "\r\n"
-  msg += "\r\n"
-  msg += body + "\r\n"
-
-  err := smtp.SendMail(serverName, auth, msg)
-  if err != nil {
-    return err
-  }
-  return nil
-}
-
-func main() {
-  recipient := "user@example.com"
-  subject := "Your Account Update"
-  body := "Hello, your account has been updated."
-
-  err := sendEmail(recipient, subject, body)
-  if err != nil {
-    fmt.Println("Error sending email:", err)
-  } else {
-    fmt.Println("Email sent successfully!")
-  }
-}
+func mail(w http.ResponseWriter, r *http.Request) {
+	host := r.Header.Get("Host")
+	token := backend.getUserSecretResetToken(email)
+	body := "Click to reset password: " + host + "/" + token
+	smtp.SendMail("test.test", nil, "from@from.com", nil, []byte(body))

9eccd4f55e2a5a99d829191d453c812cafb989e3
pgurudatta <89186176+pgurudatta@users.noreply.github.com>
2024-05-07 12:15:33 +0530

    Update useOfInsufficientRandomValue.go

--- Go/TruePositive/CWE-89/useOfInsufficientRandomValue.go
+++ Go/TruePositive/CWE-89/useOfInsufficientRandomValue.go
@@ -1,3 +1,8 @@
+/*
+Sample code for vulnerable type: Use of Insufficiently Random Values
+CWE : CWE-330
+Description : Use of Insufficiently Random Values
+*/
 package main
 
 import (
@@ -9,7 +14,11 @@ var charset = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345
 func generatePassword() string {
 	s := make([]rune, 20)
 	for i := range s {
-		s[i] = charset[rand.Intn(len(charset))]
+		s[i] = charset[rand.Intn(len(charset))]  //source and sink
 	}
 	return string(s)
+}
+func main() {
+        password := generatePassword()
+        println(password)
 }

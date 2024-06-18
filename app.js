9c70eecd15b13bd3d5acd5c60689bd98e1b191d3
pgurudatta <89186176+pgurudatta@users.noreply.github.com>
2024-06-18 15:53:09 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+var os = require("os");
+var hostname = os.hostname();
+var networkInterfaces = os.networkInterfaces();
+var username = "abc";
+var pwd = "12345678";
+const app = express()
+const port = 8000
+
+app.get('/', (req, res) => {
+  res.send(`Hello World 777, hostname=${hostname}, IP=${networkInterfaces.eth0[0].address}`)
+})
+
+app.listen(port, () => {
+  console.log(`App is running on port ${port}.`)
+})
